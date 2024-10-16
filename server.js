4095c239e1f0d148ebef5c1a82531b94649cf5bf
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-16 20:28:54 +0300

    Added limit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import injectRoutes from './routes';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: '200mb' }));
 
 injectRoutes(app);
 
