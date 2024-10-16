0fa43ce49c9ed45ee46672efcb14048012ee5680
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-16 20:15:03 +0300

    Added users post request

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+/* eslint-disable import/no-named-as-default */
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+export default class UsersController {
+  static async postNew(req, res) {
+    console.log(req.body);
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    if (!email) {
+      console.log(email);
+      res.status(400).json({ error: 'Missing email' });
+      return;
+    }
+
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const user = await (await dbClient.usersCollection()).findOne({ email });
+
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
+    }
+    const insertionInfo = await (
+      await dbClient.usersCollection()
+    ).insertOne({ email, password: sha1(password) });
+    const userId = insertionInfo.insertedId.toString();
+
+    res.status(201).json({ email, id: userId });
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+
+  api.post('/users', UsersController.postNew);
 };
 
 export default injectRoutes;
