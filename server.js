6460e621f35299f2ad2a2be9b2eea95741b48993
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-16 20:23:15 +0300

    Fixed express import structure

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-import { express } from 'express';
+import express from 'express';
 import injectRoutes from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json()); // Converts the incoming JSON to an Obj
+app.use(express.json({})); // Converts the incoming JSON to an Obj
 
 injectRoutes(app);
 
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
