0864920a5d6433a884ef20b3a24d806ba6344fed
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-16 21:50:20 +0300

    finished task 5 and 6

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,20 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 
   api.post('/users', UsersController.postNew);
-  api.get('/users/me', UsersController.getMe);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
-  api.get('/connect', AuthController.getConnect);
-  api.get('/disconnect', AuthController.getDisconnect);
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
 };
 
 export default injectRoutes;
