58ca92bf72b573f847c9556d2d3707327919369f
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-16 22:34:41 +0300

    minor fix

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ import injectRoutes from './routes';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json({ limit: '200mb' }));
-
 injectRoutes(app);
+
+app.use(express.json({ limit: '200mb' }));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
