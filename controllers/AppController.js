28a56737e7d892737ff7cdfce536db18335444f1
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-16 20:06:14 +0300

    First api

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(req, res) {
+    res.status(200).json({
+      users: dbClient.nbUsers(),
+      files: dbClient.nbFiles(),
+    });
+  }
+}
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 import { express } from 'express';
-import { router } from './routes/index';
+import injectRoutes from './routes';
 
 const app = express();
-const PORT = parseInt(process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5000;
 
-app.use('/', router); // Binds routes from local file.
 app.use(express.json()); // Converts the incoming JSON to an Obj
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+injectRoutes(app);
 
-export default app;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
